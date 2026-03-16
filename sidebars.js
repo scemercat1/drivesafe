dd89ad204b65ded884f3fc13f3b3515e723b2e83
Joe <business.tytuslemon@gmail.com>
2026-03-16 14:45:33 +0200

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -1,14 +1,8 @@
 module.exports = {
-  docs: [
-    {
-      type: 'category',
-      label: 'DriveSafe Docs',
-      items: [
-        'penalties',     // docs/penalties.md
-        'traffic-laws',  // docs/traffic-laws.md
-        'safe-driving',  // docs/safe-driving.md
-        'faq',           // docs/faq.md
-      ],
-    },
+  tutorialSidebar: [
+    'penalties',      // docs/penalties.md
+    'traffic-laws',   // docs/traffic-laws.md
+    'safe-driving',   // docs/safe-driving.md
+    'faq',            // docs/faq.md
   ],
 };
